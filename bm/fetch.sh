f2e7180589cfed53cd49348a497889ae13c1720c
Leonardo M. Ramé <martinrame@yahoo.com>
2014-11-20 18:32:15 -0300

    Added getch.sh

---
+++ bm/fetch.sh
@@ -0,0 +1,28 @@
+cookies() {
+    res=$1;
+    echo "${res}"
+    if [ "$res" = "Please read cookies and try again." ]; then
+      echo "Reading Cookies."
+      casper/bin/casperjs ./cookies.js
+      echo "After reading cookies."
+      # una vez que lee las cookies lee fecha y mercado
+      echo "Reading FechaYMercado..."
+      ../datafiller/datafiller --fechaymercado;
+      echo "After reading FechaYMercado."
+    fi
+    if [ "$res" = "Please read fechaymercado again." ]; then
+      echo "Reading FechaYMercado..."
+      ../datafiller/datafiller --fechaymercado;
+      echo "After reading FechaYMercado."
+    fi
+}
+
+while true; do 
+  echo "Fetching lideres..."
+  res="$(../datafiller/datafiller --lideres)"
+  cookies "${res}"
+  echo "Fetching opciones..."
+  res="$(../datafiller/datafiller --opciones)"
+  cookies "${res}"
+  sleep 2; 
+done
