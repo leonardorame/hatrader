3264df5a9c9e771c53b769ebe404cdd8fc8cb25e
Leonardo M. Ramé <martinrame@yahoo.com>
2014-12-09 07:56:40 -0300

    Cambios

--- db/spikes.sql
+++ db/spikes.sql
@@ -1,49 +1,28 @@
-﻿drop table tmp_test;
-create temp table tmp_test(id serial, value numeric, primary key(id));
-insert into tmp_test(value) values(47.58);
-insert into tmp_test(value) values(47.78);
-insert into tmp_test(value) values(48.09);
-insert into tmp_test(value) values(47.52);
-insert into tmp_test(value) values(48.47);
-insert into tmp_test(value) values(48.38);
-insert into tmp_test(value) values(49.30);
-insert into tmp_test(value) values(49.61);
-insert into tmp_test(value) values(50.03);
-insert into tmp_test(value) values(51.65);
-insert into tmp_test(value) values(51.65);
-insert into tmp_test(value) values(51.57);
-insert into tmp_test(value) values(50.60);
-insert into tmp_test(value) values(50.45);
-insert into tmp_test(value) values(50.83);
-insert into tmp_test(value) values(51.08);
-insert into tmp_test(value) values(51.26);
-insert into tmp_test(value) values(50.89);
-insert into tmp_test(value) values(50.51);
-insert into tmp_test(value) values(51.42);
-insert into tmp_test(value) values(52.09);
-insert into tmp_test(value) values(55.83);
-insert into tmp_test(value) values(55.79);
-insert into tmp_test(value) values(56.20);
-
-with foo as (
+﻿with foo as (
   select 
-    id,
-    value,
-    coalesce(value - lag(value, 1) over (order by id), 0) as "Price Change",
-    coalesce(lag(value, -1) over (order by id) - value, 0) as "Next Price Change",
-    ln(value / lag(value, 1) over (order by id)) as "Log Change"
-  from tmp_test
-  order by id limit 20 offset 1
+    fecha,
+    close,
+    coalesce(close - lag(close, -1) over (order by fecha desc), 0) as "Price Change",
+    coalesce(ln(close / lag(close, -1) over (order by fecha desc)), 0) as "Log Change"
+  from acciones_daily
+  where idsimbolo = 65
+  order by fecha desc limit 25
 ),
-std_dev as (select round(stddev(foo."Log Change"), 4) as stddev from foo)
+std_dev as (
+  select 
+    fecha,
+    stddev(foo."Log Change") over (rows between 19 preceding and current row) as stddev
+    from foo
+)
 select
-  foo.id,
-  foo.value as "Close",
+  foo.fecha,
+  foo.close as "Close",
   round(foo."Price Change", 2) as "Price Change",
-  round(foo."Log Change", 3) as "Log Change",
-  round(foo."Next Price Change", 2) as "Next Price Change",
+  round(foo."Log Change", 4) as "Log Change",
   round(std_dev.stddev, 4) as "StdDev Log Change",
-  round(foo.value*std_dev.stddev, 3) as "1 StdDev",
-  round((foo."Next Price Change" / (foo.value*std_dev.stddev)), 2) as "Current Spike"
+  round(foo.close*std_dev.stddev, 3) as "1 StdDev",
+  round(lag(foo.close*std_dev.stddev, -1) over (), 3) as "Prev 1 StdDev",
+  round(foo."Price Change" / lag(foo.close*std_dev.stddev, -1) over (), 2) as "Current Spike"
 from std_dev, foo
-order by foo.id desc limit 1
+where std_dev.fecha = foo.fecha
+order by foo.fecha;
